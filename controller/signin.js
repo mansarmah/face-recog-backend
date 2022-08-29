0615f57ccfdd4ca3201ee7016cb2fc9f3f3d5629
Manabendra Sarmah <manabendra.hybrutos@gmail.com>
2022-08-29 23:09:28 +0530

    code refinement

---
+++ controller/signin.js
@@ -0,0 +1,20 @@
+const handleSignIn = (req, res, db, bcrypt) => {
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const valid = bcrypt.compareSync(req.body.password, data[0].hash)
+        if(valid){
+            db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then (user => {
+                res.json(user[0])
+            }).catch(err => res.status(400).json('User not found'))
+        }else{
+            res.status(400).json('Wrong credentials')
+        }
+    }).catch(err => res.status(400).json('Wrong credentials'))
+}
+
+module.exports = {
+    handleSignIn: handleSignIn
+};
