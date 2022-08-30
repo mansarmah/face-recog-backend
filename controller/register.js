897515be022ff76081fa25ee5c18d59661f3ac6b
Manabendra Sarmah <manabendra.hybrutos@gmail.com>
2022-08-30 22:22:40 +0530

    security update

--- controller/register.js
+++ controller/register.js
@@ -1,5 +1,10 @@
 const handleRegister = (req, res, db, bcrypt) => {
     const{email, name, password} = req.body;
+
+    if(!email || !password || !name){
+        return res.status(400).json('Invalid Form Submission');
+    }
+
     const hash = bcrypt.hashSync(password);
 
     db.transaction(trx => {
--- controller/signin.js
+++ controller/signin.js
@@ -1,4 +1,9 @@
 const handleSignIn = (req, res, db, bcrypt) => {
+
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json('Invalid Form Submission');
+    }
+
     db.select('email', 'hash').from('login')
     .where('email', '=', req.body.email)
     .then(data => {
