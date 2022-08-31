3cd8d81032633fe80d7b72e678e9cf573a492d2d
Manabendra Sarmah <manabendra.hybrutos@gmail.com>
2022-08-31 12:51:09 +0530

    root dir set

--- server.js
+++ server.js
@@ -25,9 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-// app.get('/', (req, res) => {
-//     res.json(database.users);
-// })
+app.get('/', (req, res) => {res.send('It is Working')});
 
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)});
 
